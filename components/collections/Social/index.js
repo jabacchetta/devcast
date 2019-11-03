53a1661bc7dab3087113bc63f34c981353ba9f70
Jason Bacchetta <jabacchetta@gmail.com>
2019-11-02 21:06:04 -0500

    Update social icons

--- components/collections/Social/index.js
+++ components/collections/Social/index.js
@@ -4,12 +4,12 @@ import Icon from '../../elements/Icon/index'
 const Social = () => (
   <ul>
     <li>
-      <a href="https://www.youtube.com/channel/UCmZ3Ev9DjxGUbQ3SwibhT-w" rel="noopener noreferrer" target="_blank">
+      <a href="https://www.facebook.com/DevCast-100336178088902/" rel="noopener noreferrer" target="_blank">
         <Icon icon="facebook" />
       </a>
     </li>
     <li>
-      <a href="https://www.youtube.com/channel/UCmZ3Ev9DjxGUbQ3SwibhT-w" rel="noopener noreferrer" target="_blank">
+      <a href="https://twitter.com/devcastcode" rel="noopener noreferrer" target="_blank">
         <Icon icon="twitter" />
       </a>
     </li>
@@ -18,12 +18,18 @@ const Social = () => (
         <Icon icon="youtube" />
       </a>
     </li>
+    <li>
+      <a href="https://www.linkedin.com/company/devcastcode" rel="noopener noreferrer" target="_blank">
+        <Icon icon="linkedin" />
+      </a>
+    </li>
     <style jsx>{`
       ul {
         margin: 0 0 0 auto;
         padding: 0;
         list-style: none;
-        left: calc(50vw - 118px);
+        left: 50%;
+        transform: translate(-148px);
         bottom: 0px;
         display: flex;
         position: absolute;
@@ -31,7 +37,7 @@ const Social = () => (
 
       @media screen and (min-width: 470px) {
         ul {
-        left: calc(50vw - 136px);
+        {/* left: calc(50vw - 136px); */}
       }
   }
 
